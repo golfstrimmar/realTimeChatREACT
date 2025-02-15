64f3590addaf648a9f8ef422e82f4950d978cd54
golfstrimmar <yushinbox@gmail.com>
2025-02-15 12:06:42 +0100

    fff

--- .history/backend/server_20250115214506.js
+++ .history/backend/server_20250115214506.js
@@ -556,7 +556,7 @@ app.use("/auth", upload.none(), authRoutes);
 // ===================================================
 // ===================================================
 // Запуск сервера
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
